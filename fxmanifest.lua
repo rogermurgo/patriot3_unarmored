8f7e970ebb54560bb3dd7c3de8eb01e70ea15b58
rmurgo <rmurgo@doonamis.es>
2023-09-01 17:39:17 +0200

    Add handling for customising

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,12 +9,12 @@ files {
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
-    --'handling.meta',    
+    'handling.meta',    
 	--'dlctext.meta',	
 	
 }
  
---data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
