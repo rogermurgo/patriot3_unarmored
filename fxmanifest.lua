7c858a832d05e287dd6a8b1655c63c5386c2a56d
rmurgo <rmurgo@doonamis.es>
2023-09-01 13:15:27 +0200

    Add car mods from patriot 3 (Removed non rp mods)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,15 +9,15 @@ files {
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
-    'handling.meta',    
-	'dlctext.meta',	
+    --'handling.meta',    
+	--'dlctext.meta',	
 	
 }
  
 --data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
---data_file 'CARCOLS_FILE' 'carcols.meta'
---data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 --data_file 'DLC_TEXT_FILE' 'dlctext.meta'
 client_script 'names.lua'
 
